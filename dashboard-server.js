2141650edbf0456afec7a5347e6636cee4d58962
JonyEpsilon <github@j-star.org>
2012-10-19 12:16:55 +0100

    More work on user management.
    Just going for the absolute minimum registration and login functions at
    the moment.

--- dashboard-server.js
+++ dashboard-server.js
@@ -22,21 +22,35 @@ app.use(app.router);
 app.use(express.static(__dirname + '/static'));
 app.enable("trust proxy");
 
+// ** Actions **
 // authentication
-app.post('/authenticate', auth.checkCredentials);
-app.get('/logout', auth.logout);
+app.post('/authenticate', auth.checkCredentials('/login.html', '/dashboard.html'));
+app.get('/logout', auth.logout('/login.html'));
+// registration
+app.post('/register', auth.register('/register.html', '/registration_success.html'));
 
-// define views
-app.get('/login.html', function (request, response) {
-    response.render('login');
-});
+// ** Views **
+// These pages are defined as views simply to take advantage of the templating to keep the page style
+// consistent
+var addTrivialView = function (path, viewName) {
+    app.get(path, function (request, response) {
+        response.render(viewName);
+    });
+};
+addTrivialView('/login.html', 'login');
+addTrivialView('/register.html', 'register');
+addTrivialView('/registration_success.html', 'registration_success')
 
-app.get('/dashboard.html', auth.requireValidUser, users.loadUser, function (request, response) {
-   response.render('dashboard',
-       {
-           user: response.locals.user
-       });
-});
+// The main dashboard view - the heart of the app
+app.get('/dashboard.html',
+    auth.requireValidUser,
+    users.loadUser,
+    function (request, response) {
+        response.render('dashboard',
+            {
+                user: response.locals.user
+            });
+    });
 
 
 // start the server
--- db/create_database.sql
+++ db/create_database.sql
@@ -2,9 +2,12 @@
 DROP TABLE users;
 CREATE TABLE users
 (
-  userid integer NOT NULL,
+  userid SERIAL,
   username text NOT NULL,
   passwordhash text,
+  email text,
+  active boolean,
+  admin boolean,
   CONSTRAINT userid_primary_key PRIMARY KEY (userid)
 )
 WITH (
