d99d57ffee2733f272e323b08a65c13d48ae4492
JonyEpsilon <github@j-star.org>
2012-12-28 12:41:51 +0000

    Add an anti-CSRF token.
    Otherwise, it would be too easy for people to write a worksheet that
    uses your dashboard cookie to do something nasty through the API. Gotta
    wonder about the decision to allow cross-domain forms to access cookies!

--- static/js/app.js
+++ static/js/app.js
@@ -18,7 +18,7 @@ $(function () {
         logout: (function () {
             $.post('/logout', function () {
                 window.location.href = '/login.html';
-            })
+            });
         }),
 
         // handler for the worksheet edit links
@@ -27,7 +27,7 @@ $(function () {
             // to edit this worksheet. It returns the authorization token that we will use to authenticate
             // with the edit server.
             var win = window.open('');
-            $.post('/worksheets/authorizeEdit/' + encodeURIComponent(data.id), function (response) {
+            postAuthenticated('/worksheets/authorizeEdit/' + encodeURIComponent(data.id), function (response) {
                 win.location.href = model.config.editServerURL + 'edit/' + encodeURIComponent(response.uuid) +
                     '/' + encodeURIComponent(response.token);
             });
@@ -48,10 +48,9 @@ $(function () {
                     {
                         label: 'Yes, delete',
                         class: 'btn-danger',
-                        callback: (function (result) {
+                        callback: (function () {
                             // contact the dashboard server to remove the worksheet and update the UI
-                            $.post('/worksheets/delete/' + encodeURIComponent(data.id), function () {
-                                $('#deleteWorksheetModal').modal('hide');
+                            postAuthenticated('/worksheets/delete/' + encodeURIComponent(data.id), function () {
                                 model.worksheets.remove(data);
                             });
                         })
@@ -62,41 +61,62 @@ $(function () {
 
         // handler for the worksheet create button
         createWorksheet: (function () {
-            bootbox.prompt("Worksheet name", function(result) {
+            bootbox.prompt("Worksheet name", function (result) {
                 if (result !== "" && result !== null) {
                     // contact the dashboard server to add the worksheet, and update the UI
-                    $.post('/worksheets/create/' + encodeURIComponent(result), function (data) {
+                    postAuthenticated('/worksheets/create/' + encodeURIComponent(result), function (data) {
                         model.worksheets.push(data.worksheet);
-                    }, 'json');
+                    });
                 }
             })
         })
     };
 
 
-    // a helper function for authenticated requests. If any of them should return with an unauthorized status, then
-    // the browser is redirected to the login page.
+    // a helper function for authenticated JSON GETs. If any of them should return with an unauthorized status, then
+    // the browser is redirected to the login page. This call will append the anti-CSRF token from the model's config
+    // as a query parameter.
     var getAuthenticatedJSON = function (url, callback) {
-        $.getJSON(url,function (data, statusText, jqXHR) {
+        $.getJSON(url + '?_csrf=' + model.config._csrf, function (data, statusText, jqXHR) {
             callback(data, statusText, jqXHR);
         }).fail(function (jqXHR) {
                 if (jqXHR.status === 401) $('#loginModal').modal();
             }
         );
     };
+    // a similar helper for authenticated POSTs.
+    var postAuthenticated = function (url, callback) {
+        $.post(url + '?_csrf=' + model.config._csrf, function (data, statusText, jqXHR) {
+            callback(data, statusText, jqXHR);
+        }, 'json').fail(function (jqXHR) {
+                if (jqXHR.status === 401) $('#loginModal').modal();
+            }
+        );
+    };
+
+    // helper function for extracting URL query parameters
+    var getQueryParameterByName = function (name) {
+        var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+    };
+
 
     // Load configuration information from the server and start the application.
     $.getJSON('/config', function (data) {
         model.config = data;
-        ko.applyBindings(model);
-    });
+        // The login page should have passed us the anti-CSRF token as a query parameter. We store it in the
+        // config, as we will need it to make any authenticated API calls.
+        model.config._csrf = getQueryParameterByName('_csrf');
 
-    // contact the server and get the user information and worksheet list
-    getAuthenticatedJSON('/userinfo', function (data) {
-        model.user(data);
-    });
-    getAuthenticatedJSON('/worksheets', function (data) {
-        model.worksheets(data);
+        // contact the server and get the user information and worksheet list
+        getAuthenticatedJSON('/userinfo', function (data) {
+            model.user(data);
+        });
+        getAuthenticatedJSON('/worksheets', function (data) {
+            model.worksheets(data);
+        });
+
+        ko.applyBindings(model);
     });
 
 
